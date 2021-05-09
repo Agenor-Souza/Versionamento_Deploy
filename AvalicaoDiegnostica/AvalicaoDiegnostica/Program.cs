e9f37f8a7a88e3c0a66bb8cb4e9ec5975d81af58
agenor16 <44754899+agenor16@users.noreply.github.com>
2021-05-09 20:22:32 -0300

    Add files via upload

---
+++ AvalicaoDiegnostica/AvalicaoDiegnostica/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AvalicaoDiegnostica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[10];
+            int busca;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("digite o " + (i+1) + "º numero");
+                n[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Qual numero deseja buscar?");
+            busca = int.Parse(Console.ReadLine());
+            
+            
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (busca == n[i])
+                {
+                    Console.WriteLine("Número " + busca + " foi encontrado com sucesso!");
+                    if (busca % 2 == 0 )
+                    {
+                        Console.WriteLine("É par");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("É impar");
+                        break;
+                    }
+                }
+                else if (busca != n[i] && i == n.Length)
+                {
+                    Console.WriteLine("O Número " + busca + " não foi encontrado");
+                }
+            }
+        }
+    }
+}
---
+++ AvalicaoDiegnostica/AvalicaoDiegnostica/atividade1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace AvalicaoDiegnostica
+{
+    class Atividade1
+    {
+        static void Main(string[] args)
+        {
+            float[] salario = new float[10];
+            int[] anos = new int[10];
+            int reajuste = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("digite o salário do " + (i+1) + "º funcionário");
+                salario[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ha quanto tempo ele esta na empresa?");
+                anos[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Os salários reajustados são: ");
+            
+            
+            for (int i = 0; i < salario.Length; i++)
+            {
+                if (anos[i] < 5)
+                {
+                    reajuste = 0;
+                }
+                else if (anos[i] == 5)
+                {
+                    reajuste = 20;
+                }
+                else if (anos[i] == 6)
+                {
+                    reajuste = 25;
+                }
+                else if (anos[i] == 7)
+                {
+                    reajuste = 30;
+                }
+                else if (anos[i] == 8)
+                {
+                    reajuste = 40;
+                }
+                else if (anos[i] == 9)
+                {
+                    reajuste = 50;
+                }
+                else if (anos[i] >= 10)
+                {
+                    reajuste = 60;
+                }
+                Console.WriteLine($"O funcionario esta na empresa a {anos[i]} anos");
+                Console.WriteLine($"O novo salário é: R${salario[i] * (1 + reajuste/100)}, reajustado em {reajuste}%");
+            }
+        }
+    }
+}
